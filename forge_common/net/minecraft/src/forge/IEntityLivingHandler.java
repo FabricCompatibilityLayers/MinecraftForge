938dd2c6d56c16b74fea94d84c3510a9fad24b5d
Caroline Bell <halotroop2288@proton.me>
2023-11-02 14:36:45 -0500

    Fix license typos and bump license version to 1.1

--- forge_common/net/minecraft/src/forge/IEntityLivingHandler.java
+++ forge_common/net/minecraft/src/forge/IEntityLivingHandler.java
@@ -1,6 +1,5 @@
 /**
- * This software is provided under the terms of the Minecraft Forge Public
- * License v1.0.
+ * This software is provided under the terms of the Minecraft Forge Public License v1.1.
  */
 
 package net.minecraft.src.forge;
