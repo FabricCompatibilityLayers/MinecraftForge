3381e2b311f3427bf74e91d3d3d6018197c0965d
Meefy777 <47612967+meefy777@users.noreply.github.com>
2023-11-05 11:57:45 -0600

    Reforged: IFuel and canHarvestBlock metadata sensitive method in Item

---
+++ forge_common/net/minecraft/src/reforged/IFuel.java
@@ -0,0 +1,11 @@
+/**
+ * This software is provided under the terms of the Minecraft Forge Public
+ * License v1.1.
+ */
+package net.minecraft.src.reforged;
+
+public interface IFuel {
+
+    public int getFuelBurnTime(int md);
+
+}
