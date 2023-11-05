3f3c53de3771e0bf25b3d0905f3eba99ec74174d
Meefy777 <47612967+meefy777@users.noreply.github.com>
2023-11-05 11:29:05 -0600

    Reforged: Added ICustomBlockDrop and Tool Changes
    -Seperated canToolHarvest into 2 seperate methods for more versatility when coding.
    -Added ICustomBlockDrop, interface for item class. Allows id, meta and amount dropped to be changed easily (works per meta id aswell)

---
+++ forge_common/net/minecraft/src/reforged/ICustomDrop.java
@@ -0,0 +1,19 @@
+/**
+ * This software is provided under the terms of the Minecraft Forge Public
+ * License v1.1.
+ */
+package net.minecraft.src.reforged;
+
+import java.util.Random;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.ItemStack;
+
+public interface ICustomDrop {
+
+	public int getIdDropped(Block block, int md, Random rand, ItemStack stack);
+
+	public int getDamageDropped(Block block, int md, ItemStack stack);
+
+	public int getQuantityDropped(Block block, int md, Random rand, ItemStack stack);
+}
